d1beb736be40f79904bc19a8f229b78512f6c51f
ShandongUniversityMasterMa <202117815@mail.sdu.edu.cn>
2022-10-05 17:12:54 +0800

    upload .R file

---
+++ src/runPDRWH_all.R
@@ -0,0 +1,31 @@
+#run PDRWH in five different cohorts
+
+getwd()
+source("PDRWH_software.R")
+
+cancer <- c("BRCA","KIRC","LIHC","GBM","STAD")
+
+for (i in 1:length(cancer)) {
+  cancer_type <- cancer[i]
+  print("----------------------------------------------------")
+  print(paste0("Run PDRWH for cancer type:",cancer_type,"..."))
+  cat("\n\n")
+  
+  snv <- paste0("../data/",cancer_type,"/",cancer_type,"_mc3_gene_level.txt")
+  outfile_dir <- paste0("../out/",cancer_type,"/")
+  PDRWHscore(cancer_type,snv,"../data/STRINGv10.txt",outfile_dir)
+  resultTransform(cancer_type,outfile_dir,outfile_dir)
+  
+  print("----------------------------------------------------")
+  print(paste0("PDRWH for ",cancer_type,":  Achieved!"))
+  print("----------------------------------------------------")
+  cat("\n\n")
+  if (i==length(cancer)) {
+    print("----------------------------------------------------")
+    print(paste0("Congratrulations! All achieved!"))
+    print("----------------------------------------------------")
+  }
+}
+
+
+
