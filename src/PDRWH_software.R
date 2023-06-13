9a41ebc862272d86a6775307ac0fd9a7dc8c2685
ShandongUniversityMasterMa <202117815@mail.sdu.edu.cn>
2023-06-13 19:58:08 +0800

    Update PDRWH_software.R

--- src/PDRWH_software.R
+++ src/PDRWH_software.R
@@ -134,6 +134,68 @@ PDRWHscore=function(cancer_type,mut_data_file,network_file,outfile_dir){
   #print("Congratrulations!")
 }
 
+get_top_list <- function(PDRWH_score,top_num){
+  PDRWH_score <- as.matrix(PDRWH_score)
+  top_list <- list()
+  for(i in 1:ncol(PDRWH_score)) {
+    temp <- PDRWH_score[order(PDRWH_score[,i],decreasing = T),i]
+    target_genes <- names(temp)[which(temp!=0)]
+    if(top_num < length(target_genes)){
+      top_list[[i]] <- target_genes[1:top_num]
+    }else{
+      top_list[[i]] <- target_genes
+    }
+    
+  }
+  return(top_list)
+}
+
+top_condorcet <- function(top_list){
+  genes <- unique(unlist(top_list))
+  print(length(genes))
+  condorcet_mat <- matrix(0,length(genes),length(genes),dimnames = list(genes,genes))
+  
+  for(K in 1:(length(top_list))) {
+    gene_temp_list <- top_list[[K]]
+    if (length(gene_temp_list) < 2) {next}
+    for(i in 1:(length(gene_temp_list) - 1)) {
+      gene_i <- gene_temp_list[i]
+      for (j in (i + 1):length(gene_temp_list)) {
+        print(paste0("Sample: ",K," + ","gene_",i," + ","gene_",j))
+        gene_j <- gene_temp_list[j]
+        condorcet_mat[gene_i,gene_j] <- condorcet_mat[gene_i,gene_j] + 1
+      }
+    }
+  }
+  
+  return(condorcet_mat)
+}
+
+PDRWH_cohort_score <- function(input_dir){
+  PDRWH_score <- read.table(paste0(input_dir,"/PDRWH.txt"))
+  top_list <- get_top_list(PDRWH_score,300)
+  condorcet_mat <- top_condorcet(top_list)
+  
+  A <- (condorcet_mat > 0) + 0
+  Degree_v <- apply(A,2,sum)
+  for (i in 1:length(Degree_v)) {
+    if (Degree_v[i] == 0) {Degree_v[i] <- 1}
+  }
+  D_v_inverse <- diag(1/Degree_v)
+  
+  X <- matrix(1/length(rownames(A)),length(rownames(A)),1)
+  rownames(X) <- rownames(A)
+  
+  for (i in 1:30) {
+    DX <- D_v_inverse %*% X
+    X <- 0.9*A %*% DX + 0.1*X
+  }
+  gene_ordered <- X[order(X,decreasing = T),,drop=F]
+  
+  write.table(gene_ordered,paste0(input_dir,"/PDRWH_cohort.txt"),
+              quote = F,sep = "\t",row.names = T,col.names = T)
+}
+
 resultTransform=function(cancer_type,input_dir,outfile_dir){
   load(paste0(input_dir,cancer_type,".Rdata"))
   
@@ -150,7 +212,8 @@ resultTransform=function(cancer_type,input_dir,outfile_dir){
     for (j in (1:length(rownames(Importance_Score[[i]])))) {
       res_matrix[rownames(Importance_Score[[i]])[j],i] <- Importance_Score[[i]][j,]
     }
-  }  
+  }
   
   write.table(res_matrix,paste0(outfile_dir,"/PDRWH.txt"),quote = T,sep="\t")
+  PDRWH_cohort_score(outfile_dir)
 }
