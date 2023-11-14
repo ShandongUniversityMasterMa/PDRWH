b397a04a7238c02271e62464aaab39f4e52cce44
ShandongUniversityMasterMa <202117815@mail.sdu.edu.cn>
2023-11-14 11:52:27 +0800

    Update runPDRWH_all.R

--- src/runPDRWH_all.R
+++ src/runPDRWH_all.R
@@ -1,6 +1,4 @@
 #run PDRWH in five different cohorts
-
-getwd()
 source("PDRWH_software.R")
 
 cancer <- c("BRCA","KIRC","LIHC","GBM","STAD")
@@ -11,11 +9,12 @@ for (i in 1:length(cancer)) {
   print(paste0("Run PDRWH for cancer type:",cancer_type,"..."))
   cat("\n\n")
   
-  snv <- paste0("../data/",cancer_type,"/",cancer_type,"_mc3_gene_level.txt")
+  snv_file <- paste0("../data/",cancer_type,"/",cancer_type,"_mc3_gene_level.txt")
+  exp_file <- paste0("../data/",cancer_type,"/TCGA-",cancer_type,".htseq_counts.txt")
   outfile_dir <- paste0("../out/",cancer_type,"/")
-  PDRWHscore(cancer_type,snv,"../data/STRINGv10.txt",outfile_dir)
+  PDRWHscore(cancer_type,snv_file,exp_file,"../data/STRINGv10.txt",outfile_dir)
   resultTransform(cancer_type,outfile_dir,outfile_dir)
-  # PDRWH_cohort_score(outfile_dir)
+  PDRWH_cohort_score(outfile_dir)
   
   print("----------------------------------------------------")
   print(paste0("PDRWH for ",cancer_type,":  Achieved!"))
@@ -27,6 +26,4 @@ for (i in 1:length(cancer)) {
     print("----------------------------------------------------")
   }
 }
-
-
 
